c3b351e932c85b727482409b2825fea1a8f26067
Jrm1992 <marquesjoseroberto@proton.me>
2024-04-16 20:30:32 -0300

    feat: SubmitButton component

---
+++ src/components/ModalComment/submitButton.jsx
@@ -0,0 +1,16 @@
+import { useFormStatus } from 'react-dom'
+import { Button } from '../Button'
+import { ArrowFoward } from '../icons/ArrowFoward'
+
+export const SubmitButton = ({ text }) => {
+  const { pending } = useFormStatus()
+  return (
+    <div>
+      <Button disabled={pending} >
+        { pending ? <Spinner /> : text }
+        {' '}
+        <ArrowFoward />
+      </Button>
+    </div>
+  )
+}
