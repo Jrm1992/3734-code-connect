d9a90a1f2eb87128d9ba09cf0fc827443367b2c4
Jrm1992 <marquesjoseroberto@proton.me>
2024-04-16 20:30:10 -0300

    feat: created ModalComment component

--- src/components/ModalComment/index.jsx
+++ src/components/ModalComment/index.jsx
@@ -4,13 +4,23 @@ import { useRef } from "react"
 import { IconButton } from "../IconButton"
 import { Chat } from "../icons/Chat"
 import { Modal } from "../Modal"
+import { SubmitButton } from "../ModalComment/submitButton"
+import styles from './modalComment.module.css'
 
 export const ModalComment = () => {
   const modalRef = useRef(null)
   return (
     <>
       <Modal ref={modalRef}>
-        <h1>Modal</h1>
+        <form className={styles.form} action="">
+          <h2 className={styles.title}>Deixe seu comentário sobre o post:</h2>
+          <textarea
+            name="comment"
+            placeholder="Escreva um comentário..."
+            className={styles.text_area}
+          />
+          <SubmitButton text="Comentar" type="submit" />
+        </form>
       </Modal>
       <IconButton
         onClick={() => modalRef.current?.openModal()}
